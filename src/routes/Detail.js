7c7ba41f372969cab5037370ab00b375e4adafbb
Daejin Lee <daejlee@c8r2s3.42seoul.kr>
2023-05-20 06:09:42 +0900

    first React app finished

---
+++ src/routes/Detail.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import YouTube from 'react-youtube';
+import styles from "./Detail.module.css"; // npm install react-youtube
+//now we can get id !!
+
+function Detail(){
+	const [loading, setLoading] = useState(true);
+	const { id } = useParams();
+	const [movie, setMovie] = useState([]);
+	const [stars, setStars] = useState("");
+	const getStars = (rating) => {
+		let stars = "";
+		let i = 1;
+		for (i; i < rating / 2; i++)
+			stars += '★';
+		i--;
+		if (rating - i * 2 >= 0.5)
+			stars += '☆';
+		return stars;
+	};
+	const getMovie = async () => {
+		const json = await (await 
+		fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+		)).json();
+		console.log(json.data.movie);
+		setMovie(json.data.movie);
+		setStars(getStars(json.data.movie.rating));
+		setLoading(false);
+		};
+	useEffect(() => {
+		getMovie();
+	});
+	return (
+	<div>
+		{loading ? (
+		<h1>Detail page loading...</h1>
+		) : (
+		<div>
+				<a href={movie.url} target="_blank" rel="noreferrer">
+					<h1>{movie.title_long}</h1>
+				</a>
+				<img src={movie.large_cover_image} alt={movie.title} href={movie.url}/>
+				<hr/>
+				<h2>About this movie... 🧐</h2>
+				<p className={styles.description}>{movie.description_full}</p>
+				<h2>Rating : {stars}</h2>
+				<h2>Genres : </h2>
+				<ul className={styles.genre}>
+					{movie.genres.map((g) => (
+					<li key={g}>{g}</li>
+					))}
+				</ul>
+				<h2>Runtime : {parseInt(movie.runtime / 60)}hr {movie.runtime % 60}min</h2>
+				<h2>Language : {movie.language}</h2>
+				<h2>Youtube Trailer : </h2>
+				<YouTube
+					videoId={movie.yt_trailer_code}
+					opts={{width: "650", height: "500"}}
+				/>
+		</div>)
+		}
+	</div>
+	);
+}
+//challenge: add loading, and show movie details by putting json as state
+export default Detail;
---
+++ src/routes/Home.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie"
+
+function Home(){
+	const [loading, setLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+	const [minScore, setMinScore] = useState(0);
+	const [curScore, setCurScore] = useState(5);
+	const onSubmitt = (event) => {
+		event.preventDefault();
+		if (minScore <= 0 || minScore >= 10){
+			setCurScore(0);
+			return ;
+		}
+		setCurScore(minScore);
+		setLoading(true);
+		fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=${minScore}&sort_by=year`)
+		.then((response) => response.json())
+		.then((json) => {
+			setMovies(json.data.movies);
+			setLoading(false);
+		});
+	}
+	useEffect(() => {
+		fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=5.0&sort_by=year`)
+		.then((response) => response.json())
+		.then((json) => {
+			setMovies(json.data.movies);
+			setLoading(false);
+		});
+	}, []);
+	console.log(movies);
+	return (
+	<div>
+		{loading ? (
+		<h1>Loading...</h1>
+		) : (
+		<div>
+			<h1>🍿 Welcome to the Theatre! 🍿</h1>
+			<hr/>
+			<form onSubmit={onSubmitt}>
+				<label htmlFor="minScore">Minimum Rating : </label>
+				<input
+					id="minScore"
+					type="number"
+					value={minScore}
+					onChange={(e) => setMinScore(e.target.value)}
+					placeholder="between 0 and 10"
+					required
+				/>
+				<button type="submit">Search</button>
+			</form>
+			{curScore !== 0 ? (
+			<h2>You are viewing movies that rated higher than {curScore}.</h2>) : (
+			<h2>Invalid input. Please enter a number between 0 and 10.</h2>
+			)}
+			<hr/>
+			{movies.map((movie) => (
+				<Movie
+				key={movie.id} //when rendering comps inside of map
+				id={movie.id}
+				coverImg={movie.medium_cover_image}
+				title={movie.title}
+				summary={movie.summary}
+				genres={movie.genres}
+				/>
+			))}
+		</div>
+		)}
+	</div>
+	);
+}
+
+export default Home;
